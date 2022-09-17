3076d5544a75df1a8704cb288cb67fa56affa6fe
Tabassum-Khan01 <Tabassum.Khan@yahoo.com>
2022-09-17 14:44:05 -0500

    Tc_01
    Test case 1.

---
+++ src/test/Tc_01.java
@@ -0,0 +1,15 @@
+//this code syncs with github 'automation' repository
+
+
+package test;
+
+public class Tc_01 {
+
+	public static void main(String[] args) {
+
+		System.out.println("test case 01.");
+	
+		
+	}
+
+}
